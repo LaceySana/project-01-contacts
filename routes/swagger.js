8591c2835e50641e09d343761ef2e29e3f9eedec
LaceySana <laceysanabria@gmail.com>
2026-03-18 15:02:56 -0600

    fixed swagger router

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-router.get('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;

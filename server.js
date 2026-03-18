0b19cb6e17793e33ec599f54e745a7f4161649f5
LaceySana <laceysanabria@gmail.com>
2026-03-18 14:42:43 -0600

    moved swagger routes to separate file. edited prettier end of line config

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const app = express();
 require('dotenv').config();
 const mongodb = require('./database/contacts');
 const contactsRoute = require('./routes/contacts');
+const swaggerRoute = require('./routes/swagger');
 const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 9000;
 
@@ -15,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/', require('./routes'));
 app.use('/contacts', contactsRoute);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerRoute);
 
 mongodb.initDb((err) => {
     if (err) {

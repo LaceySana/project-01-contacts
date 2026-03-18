bbdf4c9e02e6ceb143a7632dcea643f8cca0a9fe
LaceySana <laceysanabria@gmail.com>
2026-03-18 17:06:08 -0600

    api-docs route unignored

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 
-// eslint-disable-next-line no-unused-vars
-router.use('/api-docs', (req, res) => {
-    // #swagger.ignore = true
-    return require('./swagger');
-});
+router.use('/api-docs', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
